08e452bfa3055ab3d8dd9504a58b8b2ee671b50f
Tobias <tobias.gooney@icloud.com>
2018-09-19 00:35:07 +0100

    Remove Uneeded Items for CLI
    - Removed the uneeded items from the Vue Cli initial set up
    - Added a basic structure for feed route

---
+++ src/pages/feed/feed.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="feed">
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import feeditem from '@/components/feed/feed-item.vue'
+
+export default {
+  name: 'feed',
+  components: {
+    feeditem
+  }
+}
+</script>
