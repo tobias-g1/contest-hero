814c63a3710e21bfd96f97ceeddd57ae9848401d
Tobias <tobias.gooney@icloud.com>
2018-09-19 22:28:43 +0100

    Create nav bar styling, add route for create contest

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import feed from './pages/feed/feed.vue'
+import createcontest from './pages/create-contest.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: feed
+    },
+    {
+      path: '/create-contest',
+      name: 'create-contest',
+      component: createcontest
     }
   ]
 })
