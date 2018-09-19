95d0cf1a6ff7920f86009bd226fd423f0d35ce37
Tobias <tobias.gooney@icloud.com>
2018-09-19 16:57:50 +0100

    Basic version of posts page getting posts from dSteem
    - Add Dsteem
    - Non production CSS for basic layout
    - Get posts in view

---
+++ src/components/feed/feed-item.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="feed-item">
+        <img v-bind:src="img" alt="">
+        <h3>{{title}}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'feed-item',
+        props: {
+            title: String,
+            img: String
+        }
+    }
+</script>
+
+<style scoped>
+    .feed-item {
+        background: white;
+            width: 30%;
+            display: inline-flex;
+            flex-direction: column;
+            min-width: 250px;
+            margin-right: 15px;
+    }
+
+    .feed-item img {
+    width: 65%;
+    }
+</style>
+
--- src/pages/feed/feed.vue
+++ src/pages/feed/feed.vue
@@ -1,17 +1,74 @@
 <template>
   <div class="feed">
-
+    <div class="filter-options">
+      <h3>Contests</h3>
+          <ul>
+            <li>Option</li>
+            <li>Option</li>
+            <li>Option</li>
+            <li>Option</li>
+            <li>Option</li>
+            <li>Option</li>
+          </ul>
+    </div>
+    <div class="posts-container">
+      <feeditem v-for="(messages, index) in messages" :key="index" :title="messages.title" :img="imageTemp" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import feeditem from '@/components/feed/feed-item.vue'
-
-export default {
-  name: 'feed',
-  components: {
-    feeditem
+  // @ is an alias to /src
+  import feeditem from '@/components/feed/feed-item.vue'
+  import {
+    Client
+  } from 'dsteem'
+  
+  const client = new Client('https://api.steemit.com')
+  
+  export default {
+    name: 'feed',
+    components: {
+      feeditem
+    },
+    data() {
+      return {
+        messages: [],
+        imageTemp: 'https://e-fisiomedic.com/wp-content/uploads/2013/11/default-placeholder-1024x1024.png'
+      }
+    },
+    computed: {
+      image: function() {
+        returnthis.messages[0].json_metadata
+      }
+    },
+    mounted() {
+      this.getPostsByTag();
+    },
+    methods: {
+      getPostsByTag: function() {
+        client.database.getDiscussions('created', {
+          tag: 'utopian-io',
+          limit: 100
+        }).then(discussions => {
+          this.messages = discussions
+        })
+      }
+    }
   }
-}
 </script>
+
+<style scoped>
+  .feed {
+    width: 100%;
+    display: inline-flex;
+  }
+  
+  .filter-options {
+    width: 30%;
+  }
+  
+  .posts-container {
+    width: 66%;
+  }
+</style>
