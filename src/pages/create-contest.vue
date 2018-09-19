e8b1ca4c3981a1d2adaa0b9b2e3211a390e45d5b
Tobias <tobias.gooney@icloud.com>
2018-09-19 23:35:26 +0100

    Add SimpleMDE, Add Form Validation, Form Layout
    - Add SimpleMDE for markdown editor
    - Form Validation on required fields with warning messages
    - Form CSS
    - Added tags, but still hard coded second tag included.

--- src/pages/create-contest.vue
+++ src/pages/create-contest.vue
@@ -1,22 +1,168 @@
-<template>
 
+<template>
+    <el-row :gutter="20">
+        <h1> Create a contest </h1>
+        <el-form :model="ruleForm" :label-position="labelPosition" :rules="rules" ref="ruleForm" class="demo-ruleForm">
+            <el-col :span="20">
+                <el-form-item label="Contest Title" prop="title">
+                    <el-input v-model="ruleForm.title" placeholder="Enter a title"></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :span="4">
+                <el-form-item label="Contest Type" prop="type">
+                    <el-select v-model="ruleForm.type" placeholder="Select Type">
+                        <el-option label="Writing" value="Writing"></el-option>
+                        <el-option label="Design" value="Design"></el-option>
+                    </el-select>
+                </el-form-item>
+            </el-col>
+            <el-col :span="24">
+                <el-form-item label="Contest Body" prop="body">
+                    <markdownEditor v-model="ruleForm.body" />
+                </el-form-item>
+            </el-col>
+            <el-col :span="12">
+                <el-form-item label="Tags">
+                    <el-tag>Contest-Hero</el-tag>
+                    <el-tag>Contest-Hero-Writing</el-tag>
+                    <el-tag :key="tag" v-for="tag in ruleForm.dynamicTags" closable :disable-transitions="false" @close="handleClose(tag)">
+                        {{tag}}
+                    </el-tag>
+                    <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="mini" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm">
+                    </el-input>
+                    <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+                </el-form-item>
+            </el-col>
+            <el-col :span="12">
+                <el-form-item label="Deadline" required>
+                    <el-form-item prop="deadline">
+                        <el-date-picker type="date" placeholder="Select Deadline" v-model="ruleForm.deadline" style="width: 100%;"></el-date-picker>
+                    </el-form-item>
+                </el-form-item>
+            </el-col>
+            <el-col :span="24">
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
+                    <el-button @click="resetForm('ruleForm')">Reset</el-button>
+                </el-form-item>
+            </el-col>
+        </el-form>
+    </el-row>
 </template>
 
 <script>
-  
-  export default {
-    name: 'create-contest',
-    components: {
+    import markdownEditor from 'vue-simplemde/src/markdown-editor'
     
-    },
-    data() {
-      return {
+    export default {
+        data() {
+            return {
+                labelPosition: 'top',
+                inputVisible: false,
+                inputValue: '',
+                ruleForm: {
+                    title: '',
+                    type: '',
+                    deadline: '',
+                    resource: '',
+                    body: '',
+                    dynamicTags: []
+                },
+                rules: {
+                    title: [{
+                            required: true,
+                            message: 'Please enter a title',
+                            trigger: 'blur'
+                        },
+                        {
+                            min: 1,
+                            max: 100,
+                            message: 'Length should be 3 to 5',
+                            trigger: 'blur'
+                        }
+                    ],
+                    type: [{
+                        required: true,
+                        message: 'Please select a contest category',
+                        trigger: 'change'
+                    }],
+                    deadline: [{
+                        type: 'date',
+                        required: true,
+                        message: 'Please select a deadline',
+                        trigger: 'change'
+                    }],
+                    body: [{
+                        required: true,
+                        message: 'Please enter the body of your contest',
+                        trigger: 'blur'
+                    }]
+                }
+            };
+        },
+        components: {
+            markdownEditor
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            },
+            handleClose(tag) {
+                this.ruleForm.dynamicTags.splice(this.ruleForm.dynamicTags.indexOf(tag), 1);
+            },
     
-      }
+            showInput() {
+                this.inputVisible = true;
+                this.$nextTick(_ => {
+                    this.$refs.saveTagInput.$refs.input.focus();
+                });
+            },
+    
+            handleInputConfirm() {
+                let inputValue = this.inputValue;
+                if (inputValue) {
+                    this.ruleForm.dynamicTags.push(inputValue);
+                }
+                this.inputVisible = false;
+                this.inputValue = '';
+            }
+        }
     }
-  }
 </script>
 
-<style scoped>
-  
+<style>
+    @import '~simplemde/dist/simplemde.min.css';
+    .el-form--label-top .el-form-item__label {
+        font-weight: bold;
+        text-transform: uppercase;
+        letter-spacing: 3px;
+        font-size: 13px;
+    }
+    
+    .el-tag+.el-tag {
+        margin-left: 10px;
+    }
+    
+    .button-new-tag {
+        margin-left: 10px;
+        height: 32px;
+        line-height: 30px;
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+    
+    .input-new-tag {
+        width: 90px;
+        margin-left: 10px;
+        vertical-align: bottom;
+    }
 </style>
