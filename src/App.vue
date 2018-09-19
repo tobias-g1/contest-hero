2cd38be0d92eba0471543d1c5d103c27cfd03408
tobias-g1 <tobias.gooney@icloud.com>
2018-09-19 22:32:53 +0100

    Create nav bar styling, add route for create contest (#3)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <el-container id="app">
     <el-header>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="logo-container"><span class="text-logo"><strong>Contest</strong> Hero</span></div>
+      <div class="menu-options">
+        <router-link to="/">Home</router-link>
+        <router-link to="/create-contest">Create Contest</router-link>
+      </div>
     </el-header>
     <el-main>
       <router-view/>
@@ -20,7 +23,6 @@
   h1 {
     font-weight: bold;
     font-size: 20px;
-
   }
   
   h2 {
@@ -29,28 +31,41 @@
     letter-spacing: 3px;
   }
   
-  h3 {
-
-  }
+  h3 {}
   
-  h4 {
-
-  }
+  h4 {}
   
-  h5 {
- 
+  h5 {}
+  
+  .el-header {
+    height: auto !important;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 25px 100px !important;
+    background: white;
+    box-shadow: -1px 2px 10px #29292912;
   }
   
   .el-main {
     color: #333;
-    padding:  20px 100px !important;
-  }
-
-  .el-header {
-    background: white;
+    padding: 20px 100px !important;
   }
   
   body>.el-container {
     margin-bottom: 40px;
+  }
+  
+  .logo-container {
+    display: inline-flex;
+  }
+  
+  .menu-options {
+    display: inline-flex;
+  }
+  
+  .text-logo {
+    letter-spacing: 3px;
+    text-transform: uppercase;
   }
 </style>
---
+++ src/pages/create-contest.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+  
+  export default {
+    name: 'create-contest',
+    components: {
+    
+    },
+    data() {
+      return {
+    
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
