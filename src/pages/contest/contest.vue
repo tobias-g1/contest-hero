e015ebbfd4d64cb0e4923c6b616a399a90323812
Tobias <tobias.gooney@icloud.com>
2018-09-20 16:33:06 +0100

    Get Author Details on load and display image and bio

--- src/pages/contest/contest.vue
+++ src/pages/contest/contest.vue
@@ -1,19 +1,23 @@
 <template>
     <el-row :gutter="20">
         <el-col :span="18">
-        <h1>{{ title }} </h1>
-        <div v-html="body" class="post-container">
-        </div>
-         <el-button type="primary">Primary</el-button>
-         <h1>Comments</h1>
-         <div class="comment-container">
-This is a comment 
-         </div>
+            <h1>{{ title }} </h1>
+            <div v-html="body" class="post-container">
+            </div>
+            <el-button type="primary">Primary</el-button>
+            <h1>Comments</h1>
+            <div class="comment-container">
+                This is a comment
+            </div>
     
         </el-col>
         <el-col :span="6">
     
             <h3>About the Author</h3>
+            <div class="author-container">
+                <img v-bind:src="authorImage">
+                <p>{{authorBio}}</p>
+            </div>
             <h3>Entries</h3>
             <div class="entry-container">
                 @someone has entered this contest
@@ -36,11 +40,13 @@ This is a comment
     export default {
         name: 'contest',
         components: {
-    
+
         },
         data() {
             return {
                 author: null,
+                authorImage: null,
+                authorBio: null,
                 permlink: null,
                 title: null,
                 body: null,
@@ -63,7 +69,7 @@ This is a comment
     
                 client.database.getDiscussions('blog', {
                     tag: this.author,
-                    start_permlink: this.permlink,  
+                    start_permlink: this.permlink,
                     start_author: this.author,
                     limit: 1
                 }).then(discussions => {
@@ -72,28 +78,49 @@ This is a comment
                     this.title = discussions[0].title
                 })
     
+            },
+            getAuthorDetails(author) {
+                client.database.getAccounts([author]).then(authorDetails => {
+                    let userJson = JSON.parse(authorDetails[0].json_metadata)
+                    this.authorImage = userJson.profile.profile_image
+                    this.authorBio = userJson.profile.about
+                })
             }
         },
         mounted() {
             this.loadContent();
+            this.getAuthorDetails(this.author)
         }
     }
 </script>
 
-<style scoped>
+<style>
     .post-container {
         background: white;
         border-radius: 5px;
         padding: 15px;
     }
-
+    
+    .post-container img {
+        max-width: 100%;
+        height: auto;
+    }
+    
     .entry-container {
         background: white;
         padding: 15px;
     }
-
+    
     .comment-container {
-         background: white;
-        padding: 15px;  
+        background: white;
+        padding: 15px;
+    }
+    
+    .author-container img {
+        height: 50px;
+        width: 50px;
+        border: 2px solid white;
+        border-radius: 50px;
+        box-shadow: 0 0 4px #ccc9c9;
     }
 </style>
