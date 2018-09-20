81ba172ad0fe40aa8a754317b2d40a04bf464350
tobias-g1 <tobias.gooney@icloud.com>
2018-09-20 08:35:45 +0100

    Add Basic 404 (#4)

---
+++ src/pages/404.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+Sorry, what you're looking for couldn't be located
+    </div>
+</template>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import feed from './pages/feed/feed.vue'
 import createcontest from './pages/create-contest.vue'
+import notfound from './pages/404.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/create-contest',
       name: 'create-contest',
       component: createcontest
+    },
+    {
+      path: '*',
+      name: 'not-found',
+      component: notfound
     }
   ]
 })
