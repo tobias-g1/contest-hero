4156726a0703254014e0dfff0a0d2e69f5fe621e
Tobias <tobias.gooney@icloud.com>
2018-09-23 13:57:11 +0100

    Add vuex

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import ElementUI from 'element-ui'
 
 import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
 
 Vue.use(ElementUI)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
