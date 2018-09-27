7b3f2e45ac8e19406bc30f998e652eba3aad37bc
Tobias <tobias.gooney@icloud.com>
2018-09-27 20:00:23 +0100

    Removed reference of form action in mixin

--- src/mixins/form-actions.js
+++ src/mixins/form-actions.js
@@ -1,16 +1,6 @@
 var formActions = {
 
   methods: {
-    submitForm (formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert('submit!')
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
-    },
     resetForm (formName) {
       this.$refs[formName].resetFields()
       this.$refs[formName].dynamicTags = 0
