9e453cf291c29a82423897e4c97338184807c25d
Tobias <tobias.gooney@icloud.com>
2018-09-27 01:00:05 +0100

    Adjust date picker locale

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './app/App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en'
 import store from './store'
 
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale })
 
 new Vue({
   router,
