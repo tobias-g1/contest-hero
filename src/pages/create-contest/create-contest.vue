7e9b27553945114bc8b4c2fa120be906e83f392b
Tobias <tobias.gooney@icloud.com>
2018-09-27 19:28:55 +0100

    Add ability to create a contest post via SteemConnect

--- src/pages/create-contest/create-contest.vue
+++ src/pages/create-contest/create-contest.vue
@@ -1,17 +1,17 @@
 
 <template>
-    <el-row :gutter="20" >
+    <el-row :gutter="20">
         <h1 class="header"> <img class="small-circle" src="@/assets/gradient-circle.png" alt=""> Create a contest </h1>
         <el-form :model="contestForm" :label-position="labelPosition" :rules="rules" ref="contestForm">
             <el-col :span="24">
-                <el-form-item  label="Contest Title" prop="title">
+                <el-form-item label="Contest Title" prop="title">
                     <el-input v-model="contestForm.title" placeholder="Enter a title"></el-input>
                 </el-form-item>
             </el-col>
             <el-col :span="12">
                 <el-form-item label="Deadline" required>
                     <el-form-item prop="deadline">
-                        <el-date-picker type="date" placeholder="Select Deadline" v-model="contestForm.deadline" style="width: 100%;"></el-date-picker>
+                        <el-date-picker type="date"  placeholder="Select Deadline" v-model="contestForm.deadline" style="width: 100%;" value-format="dd/MM/yyyy"></el-date-picker>
                     </el-form-item>
                 </el-form-item>
             </el-col>
@@ -34,13 +34,13 @@
             <el-col :span="12">
                 <el-form-item label="Tags">
                     <div class="tags-container">
-                        <el-tag :key="tags" v-for="tags in fixedTags" v-show="tags">  {{ tags }} </el-tag>
-                            <el-tag :key="tag" v-for="tag in contestForm.dynamicTags" closable :disable-transitions="false" @close="handleClose(tag, contestForm)">
-                                {{tag}}
-                            </el-tag>
-                            <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm(contestForm)" @blur="handleInputConfirm">
-                            </el-input>
-                            <el-button v-else class="button-new-tag" size="small" @click="showInput" v-show="(finalTags.length < 5)">+ New Tag</el-button>
+                        <el-tag :key="tags" v-for="tags in fixedTags" v-show="tags"> {{ tags }} </el-tag>
+                        <el-tag :key="tag" v-for="tag in contestForm.dynamicTags" closable :disable-transitions="false" @close="handleClose(tag, contestForm)">
+                            {{tag}}
+                        </el-tag>
+                        <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm(contestForm)" @blur="handleInputConfirm">
+                        </el-input>
+                        <el-button v-else class="button-new-tag" size="small" @click="showInput" v-show="(finalTags.length < 5)">+ New Tag</el-button>
                     </div>
                 </el-form-item>
             </el-col>
@@ -58,6 +58,9 @@
     import markdownEditor from 'vue-simplemde/src/markdown-editor'
     import form from '@/mixins/form-actions.js'
     import tags from '@/mixins/tags.js'
+    import {
+        mapGetters
+    } from 'vuex'
     
     export default {
         data() {
@@ -84,7 +87,6 @@
                         trigger: 'change'
                     }],
                     deadline: [{
-                        type: 'date',
                         required: true,
                         message: 'Please select a deadline',
                         trigger: 'change'
@@ -109,16 +111,58 @@
         mixins: [form, tags],
         computed: {
             fixedTags: function() {
-                let fixedTags = ['contest-hero']
+                let fixedTags = ['ch-test1aaaaa']
                 fixedTags.push(this.contestForm.type)
                 return fixedTags
             },
             finalTags: function() {
                 return this.fixedTags.concat(this.contestForm.dynamicTags)
-            }
+            },
+            ...mapGetters('steemconnect', ['user']),
         },
         methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        this.createContest()
+                    } else {
+                        console.log('error submit!!')
+                        return false
+                    }
+                })
+            },
+            createContest() {
+
+                // Parent author and parentPermLink not required for submitted a post to the blockchain
     
+                let parentAuthor = ''
+                let parentPermlink = this.finalTags[0]
+
+                // Create JSON Metadata
+    
+                let jsonMetaData = {
+                    'tags': this.finalTags,
+                    'app': 'contest-hero',
+                    'contest-hero': {
+                        'type': 'contest',
+                        'deadline': this.contestForm.deadline
+                    }
+                }
+
+                // Send post via SteemConnect
+    
+                this.$steemconnect.comment(
+                    parentAuthor,
+                    parentPermlink,
+                    this.$store.state.steemconnect.user.name,
+                    'contest-hero-' + this.contestForm.title.replace(/\s/g, '-'),
+                    this.contestForm.title,
+                    this.contestForm.body,
+                    jsonMetaData,
+                    (err) => {
+                        alert(err)
+                    })
+            }
         }
     }
 </script>
