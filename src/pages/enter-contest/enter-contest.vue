91a716e5e6f0727b90ba77f0981c75b36a455e91
Tobias <tobias.gooney@icloud.com>
2018-09-28 20:54:11 +0100

    Adjust form name and data structure for validation and tags, add computed tags

--- src/pages/enter-contest/enter-contest.vue
+++ src/pages/enter-contest/enter-contest.vue
@@ -2,15 +2,15 @@
 <template>
     <el-row :gutter="20">
         <h1 class="header"> <img class="small-circle" src="@/assets/gradient-circle.png" alt=""> Enter contest </h1>
-        <el-form :model="entry" :label-position="labelPosition" :rules="rules" ref="entry" class="demo-entry">
+        <el-form :model="entryForm" :label-position="labelPosition" :rules="rules" ref="entryForm" class="demo-entry">
             <el-col :span="24">
                 <el-form-item label="Entry Title" prop="title">
-                    <el-input v-model="entry.title" placeholder="Enter a title"></el-input>
+                    <el-input v-model="entryForm.title" placeholder="Enter a title"></el-input>
                 </el-form-item>
             </el-col>
             <el-col :span="24">
                 <el-form-item label="Entry Body" prop="body">
-                    <markdownEditor v-model="entry.body" />
+                    <markdownEditor v-model="entryForm.body" />
                 </el-form-item>
             </el-col>
             <el-col :span="24">
@@ -18,19 +18,19 @@
                     <div class="tags-container">
                         <el-tag>Contest-Hero</el-tag>
                         <el-tag>Contest-Hero-Writing</el-tag>
-                        <el-tag :key="tag" v-for="tag in entry.dynamicTags" closable :disable-transitions="false" @close="handleClose(tag)">
+                        <el-tag :key="tag" v-for="tag in entryForm.dynamicTags" closable :disable-transitions="false" @close="handleClose(tag)">
                             {{tag}}
                         </el-tag>
                         <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small" @keyup.enter.native="handleInputConfirm" @blur="handleInputConfirm">
                         </el-input>
-                        <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+                        <el-button v-else class="button-new-tag" v-show="(finalTags.length < 5)" size="small" @click="showInput">+ New Tag</el-button>
                     </div>
                 </el-form-item>
             </el-col>
             <el-col :span="24">
                 <el-form-item>
                     <button @click="alert('hello')"  class="btn-fill">Enter Contest</button>
-                    <el-button @click="resetForm('entry')">Reset</el-button>
+                    <el-button @click="resetForm('entryForm')">Reset</el-button>
                 </el-form-item>
             </el-col>
         </el-form>
@@ -41,6 +41,9 @@
     import markdownEditor from 'vue-simplemde/src/markdown-editor'
     import form from '@/mixins/form-actions.js'
     import tags from '@/mixins/tags.js'
+      import {
+        mapGetters
+    } from 'vuex'
     
     export default {
         data() {
@@ -48,9 +51,9 @@
                 labelPosition: 'top',
                 inputVisible: false,
                 inputValue: '',
-                author: '',
-                permlink: '',
-                entry: {
+                contestAuthor: '',
+                contestPermlink: '',
+                entryForm: {
                     title: '',
                     body: '',
                     dynamicTags: []
@@ -73,11 +76,20 @@
             markdownEditor
         },
         mixins: [form, tags],
+        computed: {
+            fixedTags: function() {
+                return ['contest-hero']
+            },
+            finalTags: function() {
+                return this.fixedTags.concat(this.entryForm.dynamicTags)
+            },
+            ...mapGetters('steemconnect', ['user']),
+        },
         methods: {
             setDetails() {
-                this.author = this.$route.params.author
-                this.permlink = this.$route.params.permlink
-            }
+                this.contestAuthor = this.$route.params.author
+                this.contestPermlink = this.$route.params.permlink
+            },
         },
         mounted() {
             this.setDetails()
