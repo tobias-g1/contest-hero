949eae55e3b4e8160ec239cfbfc50247351c03f8
Tobias <tobias.gooney@icloud.com>
2018-09-29 17:39:00 +0100

    Grab data from route on load

--- src/pages/view-entry/view-entry.vue
+++ src/pages/view-entry/view-entry.vue
@@ -1,10 +1,42 @@
-
 <template>
 
 </template>
 
 <script>
-   
+
+    import dsteem from '@/mixins/dsteem.js'
+
+    export default {
+        name: 'view-entry',
+        mixins: [dsteem],
+        components: {
+
+        },
+        data() {
+            return {
+                post: {
+                    author: null,
+                    data: null,
+                    permlink: null,
+                    comments: []
+                }
+            }
+        },
+        methods: {
+            loadContent() {
+                this.post.author = this.$route.params.author
+                this.post.permlink = this.$route.params.permlink
+                this.loadPost(this.post.author, this.post.permlink)
+                    .then(discussions => {
+                        this.post.data = discussions[0]
+                   
+                    })
+            }
+        },
+        mounted() {
+            this.loadContent();
+        }
+    }
 </script>
 
 <style src="@/pages/view-entry/view-entry.css">
