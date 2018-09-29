f44eb81c27ebe9dc1e229e17259c0bd7d78504c2
Tobias <tobias.gooney@icloud.com>
2018-09-29 18:09:25 +0100

    Add ability to comment on an entry

--- src/pages/view-entry/view-entry.vue
+++ src/pages/view-entry/view-entry.vue
@@ -4,15 +4,23 @@
             <!-- Post Container -->
             <h1 class="header"><img class="small-circle" src="@/assets/gradient-circle.png" alt="">{{ post.data.title }}</h1>
             <div class="post-container">
-            <post :postbody="post.data.body">
-
-                
-            </post>
+                <post :postbody="post.data.body"></post>
                 <div class="tags">
-                <el-tag v-for="(tag, index) in tags" :key="index" :comment="comments">{{ tag}}</el-tag>
+                    <el-tag v-for="(tag, index) in tags" :key="index" :comment="comments">{{ tag}}</el-tag>
+                </div>
             </div>
-            </div>
-        
+            <!-- Post Comments -->
+            <h1 class="header"> <img class="small-circle" src="@/assets/gradient-circle.png" alt="">Comments</h1>
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+                <el-form-item prop="commentbody">
+                    <markdownEditor v-model="ruleForm.commentbody" />
+                </el-form-item>
+                <el-form-item>
+                    <button @click="submitForm('ruleForm')" class="btn-fill">Create Comment</button>
+                    <el-button @click="resetForm('ruleForm')">Reset</el-button>
+                </el-form-item>
+            </el-form>
+    
         </el-col>
     </el-row>
 </template>
@@ -20,12 +28,16 @@
 <script>
     import dsteem from '@/mixins/dsteem.js'
     import post from '@/components/post/post.vue'
+    import form from '@/mixins/form-actions.js'
+    import markdownEditor from 'vue-simplemde/src/markdown-editor'
     
     export default {
         name: 'view-entry',
-        mixins: [dsteem],
+        mixins: [dsteem, form],
         components: {
+            markdownEditor,
             post
+    
         },
         data() {
             return {
@@ -34,6 +46,16 @@
                     data: null,
                     permlink: null,
                     comments: []
+                },
+                ruleForm: {
+                    commentBody: ''
+                },
+                rules: {
+                    commentbody: [{
+                        required: true,
+                        message: 'Please enter a comment',
+                        trigger: 'blur'
+                    }]
                 }
             }
         },
@@ -45,7 +67,45 @@
                     .then(discussions => {
                         this.post.data = discussions[0]
                     })
-            }
+            },
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        this.submitComment()
+                    } else {
+                        console.log('error submit!!')
+                        return false
+                    }
+                })
+            },
+            submitComment() {
+    
+                this.$store.commit('setLoading', true)
+    
+                // Create JSON Metadata
+    
+                let jsonMetaData = {
+                    'app': 'contest-hero',
+                    'contest-hero': {
+                        'type': 'entry_comment'
+                    }
+                }
+    
+                // Send comment via SteemConnect
+    
+                this.$steemconnect.comment(
+                    this.post.author,
+                    this.post.permlink,
+                    this.$store.state.steemconnect.user.name,
+                    this.post.permlink + Math.floor(Math.random() * 9000000000) + 1000000000,
+                    '',
+                    this.ruleForm.commentbody,
+                    jsonMetaData).then(err => {
+                    this.$store.commit('setLoading', false)
+                    this.getComments(this.post.author, this.post.permlink)
+                    this.ruleForm.commentbody = ''
+                })
+            },
         },
         mounted() {
             this.loadContent();
