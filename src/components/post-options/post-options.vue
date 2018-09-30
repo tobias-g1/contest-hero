0b4be0bb55446e0f5b1a697e347fee2135eedb0c
Tobias <tobias.gooney@icloud.com>
2018-09-30 18:21:12 +0100

    Check if current user has already voted, add loading indicator

--- src/components/post-options/post-options.vue
+++ src/components/post-options/post-options.vue
@@ -18,40 +18,50 @@
         },
         methods: {
             vote(currentUser, author, permlink, weight) {
+                this.$store.commit('setLoading', true)
                 this.$steemconnect.vote(currentUser, author, permlink, weight)
                     .then(err => {
                         this.voted = true
+                        this.$store.commit('setLoading', false)
                     })
+            },
+            checkVote: function() {
+                for (let i = 0; i < this.post.active_votes.length; i++) {
+                    console.log(this.post.active_votes[i])
+                    if (this.post.active_votes[i].voter == this.$store.state.steemconnect.user.name) {
+                        this.voted = true
+                    }
+                }
             }
+        },
+        mounted() {
+            this.checkVote();
         }
     }
-      
 </script>
 
 <style>
-
-.stats-container {
-    display: flex;
-    align-items: center;
-}
-
-.stat-option {
-    font-size: 20px;
-    margin: 5px;
-}
-
-.vote {
-    cursor: pointer;
-}
-
-.voted {
-    color: #FF1480;
-}
-
-.post-bar {
-    display: inline-flex;
-    justify-content: space-between;
-    width: 100%;
-}
+    .stats-container {
+        display: flex;
+        align-items: center;
+    }
     
+    .stat-option {
+        font-size: 20px;
+        margin: 5px;
+    }
+    
+    .vote {
+        cursor: pointer;
+    }
+    
+    .voted {
+        color: #FF1480;
+    }
+    
+    .post-bar {
+        display: inline-flex;
+        justify-content: space-between;
+        width: 100%;
+    }
 </style>
