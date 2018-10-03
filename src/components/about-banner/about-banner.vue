4f665289b162515ccc3c86b358665b37c1c5e955
Tobias <tobias.gooney@icloud.com>
2018-10-03 20:09:12 +0100

    Move about banner css to own file

--- src/components/about-banner/about-banner.vue
+++ src/components/about-banner/about-banner.vue
@@ -3,32 +3,4 @@
     <div class="about-banner"><span>Contest Hero helps you to find, create and enter the best contests on the Steem Blockchain</span></div>
  </div>
 </template>
-<style>
-.banner-pattern {
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(45deg, #6710ff, #FF1480);
-  }
-  
-  .about-banner {
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%239C92AC' fill-opacity='0.7' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
-    min-height: 100px;
-    color: white;
-    text-align: center;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    font-size: 18px;
-    padding: 50px 15%;
-    text-transform: uppercase;
-    letter-spacing: 3px;
-    font-weight: bold;
-  }
-</style>
+<style src='@/components/about-banner/about-banner.css'></style>
