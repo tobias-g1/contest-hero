c1147d2725864e4bcf5eb9322783387198652b36
Tobias <tobias.gooney@icloud.com>
2018-10-03 22:01:37 +0100

    Force https

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const secure = require('express-force-https')
 
 // create the express app
 const app = express()
 
 // create middleware to handle the serving the app
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use(secure)
 
 // Create default port to serve the app on
 const port = process.env.PORT || 5000
