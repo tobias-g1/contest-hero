0c6fb91d0737d84475a50129a8918a9f27193e3a
Tobias <tobias.gooney@icloud.com>
2018-10-06 17:42:03 +0100

    Revert "1.1.10 (#40)"
    This reverts commit f40795ee2b6dd1fd5028d2aedcafce1643229e36.

--- src/components/about-author/about-author.vue
+++ src/components/about-author/about-author.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="author-container">
-        <img :src="authorImage" @error="setDefault">
+        <img v-bind:src="authorImage" @error="setDefault">
         <div class="author-details">
         <span class="author-name"> {{ authorName }} </span>
         <span class="author-bio "> {{ authorBio }} </span>
@@ -13,13 +13,15 @@ export default {
   name: 'about-author',
   props: {
     authorName: String,
+    authorImage: String,
     authorBio: String
   },
-  computed: {
-    authorImage: function () {
-      return `https://steemitimages.com/u/${this.authorName}/avatar`
+  methods: {
+    setDefault: function () {
+      this.authorImage = require('@/assets/profile-placeholder.png')
     }
   }
+
 }
 </script>
 
