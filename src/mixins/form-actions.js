3f4e1f6c9442a39d70c5a28a398815e7ca6027f7
Tobias <tobias.gooney@icloud.com>
2018-10-07 21:45:34 +0100

    Add quick selections for 1 day, 7 days and 30 days

--- src/mixins/form-actions.js
+++ src/mixins/form-actions.js
@@ -4,7 +4,30 @@ var formActions = {
       pickerOptions: {
         disabledDate (time) {
           return time.getTime() < Date.now() - 8.64e7
-        }
+        },
+        shortcuts: [{
+          text: '1 day',
+          onClick (picker) {
+            const date = new Date()
+            date.setTime(date.getTime() + 3600 * 1000 * 24)
+            picker.$emit('pick', date)
+          }
+        }, {
+          text: '7 days',
+          onClick (picker) {
+            const date = new Date()
+            date.setTime(date.getTime() + 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', date)
+          }
+        },
+        {
+          text: '30 days',
+          onClick (picker) {
+            const date = new Date()
+            date.setTime(date.getTime() + 3600 * 1000 * 24 * 30)
+            picker.$emit('pick', date)
+          }
+        }]
       }
     }
   },
