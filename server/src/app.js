74caeb41d67e7b583394139497c590ddef5e40ed
Tobias <tobias.gooney@icloud.com>
2018-10-14 04:26:10 +0100

    Folder Structure & Procfile Adjustments
    - Move Readme
    - Move Procfile
    - Add package.json to root
    - Add .env to client
    - Adjust Procfile for API and Client

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ app.use(entries);
 
 
 var mongoose = require('mongoose');
-mongoose.connect(`${process.env.DB}/contests`);
+mongoose.connect(`${process.env.DATABASE}/contests`);
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback){
