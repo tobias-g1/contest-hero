f40f8dff528bf86c8fe2fb1cb716b74086e9f2a8
Tobias <tobias.gooney@icloud.com>
2018-10-20 14:40:42 +0100

    Adjust contests services in line with api route changes

--- client/src/services/contests.js
+++ client/src/services/contests.js
@@ -3,18 +3,18 @@ import api from '@/services/api.js'
 export default {
 
   getContests () {
-    return api().get('contests')
+    return api().get('/contests/all')
   },
   createContest (params) {
-    return api().post('contests', params)
+    return api().post('/contests/create_contest', params)
   },
   getContestsByCategory (params) {
-    return api().get(`/contests/category/${params}`)
+    return api().get(`/contests/${params}`)
   },
   getContestByPermlink (params) {
     return api().get(`/contests/permlink/${params}`)
   },
   updateWinners (params) {
-    return api().put('contests/winners/' + params.id, params)
+    return api().put('contests/set_winners/' + params.id, params)
   }
 }
