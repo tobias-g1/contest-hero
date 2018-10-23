d87b237be6f022b8efcc805893bb8f0541c51209
Tobias <tobias.gooney@icloud.com>
2018-10-23 23:40:13 +0100

    Add tooltips to create contest

---
+++ client/src/components/tooltip/tooltip.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-tooltip class="item" effect="dark" :content="text" placement="top">
+        <i class="material-icons tool-tip"> help_outline</i>
+    </el-tooltip>
+</template>
+<script>
+export default {
+  name: 'selected-winner',
+  props: {
+    text: String
+  }
+}
+</script>
+<style>
+
+.tool-tip {
+   vertical-align: middle;
+   font-size: 22px;
+   color: #606266;
+}
+
+</style>
