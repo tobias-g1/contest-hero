74f8f8d3faa7010e3e4183be4e623628b59f4d27
tobias-g1 <tobias.gooney@icloud.com>
2018-10-23 19:56:09 +0100

    Add time stamps to entries, contests and user models (#70)

--- server/src/models/contests.model.js
+++ server/src/models/contests.model.js
@@ -1,5 +1,6 @@
 // Definitions
 
+var timestamps = require('mongoose-timestamp');
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
@@ -39,6 +40,8 @@ var ContestSchema = new Schema({
     default: false,
   }
 });
+
+ContestSchema.plugin(timestamps); // automatically adds createdAt and updatedAt timestamps
 
 // Define model
 
