d62538041ff93e36b379b61aeb96dac312bcce15
tobias-g1 <tobias.gooney@icloud.com>
2018-10-26 16:53:45 +0100

    Move comments into own component (#78)
    * Create comment panel component
    
    * Remove comment code from view-contest and replace with component
    
    * Replace comments in view-entry with comment componet
    
    * Order comments newest to oldest

---
+++ client/src/components/comment-panel/comment-panel.vue
@@ -0,0 +1,128 @@
+<template>
+    <div class="comment-panel">
+        <!-- Post Comments -->
+        <h1 class="header"> <img class="small-circle" src="@/assets/gradient-circle.png" alt="">Comments</h1>
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" @submit.native.prevent>
+            <el-form-item prop="commentbody">
+                <markdownEditor v-model="ruleForm.commentbody" />
+            </el-form-item>
+            <el-form-item>
+                <button :disabled="!this.$store.state.steemconnect.user" @click="submitForm('ruleForm')" class="btn-fill">Create Comment</button>
+                <el-button @click="resetForm('ruleForm')">Reset</el-button>
+            </el-form-item>
+        </el-form>
+        <comment v-for="(comment, index) in sortedComments" :key="index" :comment="comment" />
+    </div>
+</template>
+
+<script>
+import dsteem from '@/mixins/dsteem.js'
+import post from '@/components/post/post.vue'
+import form from '@/mixins/form-actions.js'
+import markdownEditor from 'vue-simplemde/src/markdown-editor'
+import comment from '@/components/contest-comment/contest-comment.vue'
+import postoptions from '@/components/post-options/post-options.vue'
+
+export default {
+  name: 'comment-panel',
+  mixins: [dsteem, form],
+  components: {
+    markdownEditor,
+    post,
+    comment,
+    postoptions
+  },
+  props: {
+    post: Object
+  },
+  data () {
+    return {
+      ruleForm: {
+        commentBody: ''
+      },
+      rules: {
+        commentbody: [{
+          required: true,
+          message: 'Please enter a comment',
+          trigger: 'blur'
+        }]
+      }
+    }
+  },
+  mounted () {
+    this.getComments(this.post.author, this.post.permlink)
+  },
+  methods: {
+    // Get comments from the blockchain
+    getComments (author, permlink) {
+      this.getPostComments(author, permlink)
+        .then(postComments => {
+          postComments.forEach(comment => {
+            this.getAccount(comment.author)
+              .then(commentAuthorDetails => {
+                commentAuthorDetails[0].json_metadata = JSON.parse(commentAuthorDetails[0].json_metadata)
+                comment.authorDetails = commentAuthorDetails[0]
+                this.post.comments.push(comment)
+              })
+          })
+        })
+    },
+    // Form Logic
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.submitComment()
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    submitComment () {
+      this.$store.commit('setLoading', true)
+
+      // Create JSON Metadata
+
+      let jsonMetaData = {
+        'app': 'contest-hero',
+        'format': 'markdown',
+        'contest-hero': {
+          'type': 'entry_comment'
+        },
+        'tags': [this.tags[0]]
+      }
+
+      // Send comment via SteemConnect
+
+      this.$steemconnect.comment(
+        this.post.author,
+        this.post.permlink,
+        this.$store.state.steemconnect.user.name,
+        this.post.permlink + Math.floor(Math.random() * 9000000000) + 1000000000,
+        '',
+        this.ruleForm.commentbody,
+        jsonMetaData,
+        (err) => {
+          (err) ? alert('Sorry an error has occured, please try again later or alternatively please report this issue via Github') : this.getComments(this.post.author, this.post.permlink)
+          this.$store.commit('setLoading', false)
+          this.ruleForm.commentbody = ''
+        })
+    }
+  },
+  computed: {
+    postJson: function () {
+      return JSON.parse(this.post.data.json_metadata)
+    },
+    tags: function () {
+      return this.postJson.tags
+    },
+    sortedComments: function () {
+      return _.orderBy(this.post.comments, 'created', 'desc')
+    }
+  }
+}
+</script>
+
+<style src="@/components/comment-panel/comment-panel.css">
+
+</style>
