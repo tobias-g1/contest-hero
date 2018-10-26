873bc6ac16015a5e92dc6f497bb5d5c887f10aab
Tobias <tobias.gooney@icloud.com>
2018-10-26 14:51:38 +0100

    Replace comments in view-entry with comment componet

--- client/src/pages/view-entry/view-entry.vue
+++ client/src/pages/view-entry/view-entry.vue
@@ -13,18 +13,7 @@
                     <postoptions :post="post.data" />
                 </div>
             </div>
-                <!-- Post Comments -->
-                <h1 class="header"> <img class="small-circle" src="@/assets/gradient-circle.png" alt="">Comments</h1>
-                <el-form :model="ruleForm" :rules="rules" ref="ruleForm" @submit.native.prevent>
-                    <el-form-item prop="commentbody">
-                        <markdownEditor v-model="ruleForm.commentbody" />
-                    </el-form-item>
-                    <el-form-item>
-                        <button :disabled="!this.$store.state.steemconnect.user" @click="submitForm('ruleForm')" class="btn-fill">Create Comment</button>
-                        <el-button @click="resetForm('ruleForm')">Reset</el-button>
-                    </el-form-item>
-                </el-form>
-                <comment v-for="(comments, index) in post.comments" :key="index" :comment="comments" />
+            <commentpanel :post="post"/>
         </el-col>
     </el-row>
  </el-main>
@@ -33,19 +22,16 @@
 <script>
 import dsteem from '@/mixins/dsteem.js'
 import post from '@/components/post/post.vue'
-import form from '@/mixins/form-actions.js'
-import markdownEditor from 'vue-simplemde/src/markdown-editor'
-import comment from '@/components/contest-comment/contest-comment.vue'
-import postoptions from '@/components/post-options/post-options.vue'
 import entriesService from '@/services/entries.js'
+import commentpanel from '@/components/comment-panel/comment-panel.vue'
+import postoptions from '@/components/post-options/post-options.vue'
 
 export default {
   name: 'view-entry',
-  mixins: [dsteem, form],
+  mixins: [dsteem],
   components: {
-    markdownEditor,
     post,
-    comment,
+    commentpanel,
     postoptions
   },
   data () {
@@ -56,16 +42,6 @@ export default {
         data: null,
         permlink: null,
         comments: []
-      },
-      ruleForm: {
-        commentBody: ''
-      },
-      rules: {
-        commentbody: [{
-          required: true,
-          message: 'Please enter a comment',
-          trigger: 'blur'
-        }]
       }
     }
   },
@@ -84,7 +60,6 @@ export default {
       } else {
         this.getSingleCommentBlockchain()
       }
-      this.getComments(this.post.author, this.post.permlink)
       this.$store.commit('setLoading', false)
     },
     getPostBlockchain () {
@@ -97,59 +72,6 @@ export default {
       this.getSingleComment(this.post.author, this.post.permlink)
         .then(discussions => {
           this.post.data = discussions[0]
-        })
-    },
-    getComments (author, permlink) {
-      this.getPostComments(author, permlink)
-        .then(postComments => {
-          postComments.forEach(comment => {
-            this.getAccount(comment.author)
-              .then(commentAuthorDetails => {
-                commentAuthorDetails[0].json_metadata = JSON.parse(commentAuthorDetails[0].json_metadata)
-                comment.authorDetails = commentAuthorDetails[0]
-                this.post.comments.push(comment)
-              })
-          })
-        })
-    },
-    submitForm (formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          this.submitComment()
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
-    },
-    submitComment () {
-      this.$store.commit('setLoading', true)
-
-      // Create JSON Metadata
-
-      let jsonMetaData = {
-        'app': 'contest-hero',
-        'format': 'markdown',
-        'contest-hero': {
-          'type': 'entry_comment'
-        },
-        'tags': [this.tags[0]]
-      }
-
-      // Send comment via SteemConnect
-
-      this.$steemconnect.comment(
-        this.post.author,
-        this.post.permlink,
-        this.$store.state.steemconnect.user.name,
-        this.post.permlink + Math.floor(Math.random() * 9000000000) + 1000000000,
-        '',
-        this.ruleForm.commentbody,
-        jsonMetaData,
-        (err) => {
-          (err) ? alert('Sorry an error has occured, please try again later or alternatively please report this issue via Github') : this.getComments(this.post.author, this.post.permlink)
-          this.$store.commit('setLoading', false)
-          this.ruleForm.commentbody = ''
         })
     }
   },
