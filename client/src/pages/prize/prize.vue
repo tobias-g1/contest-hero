4a876af1020df371d1f798edf299001ffa199b2e
Tobias <tobias.gooney@icloud.com>
2018-10-31 21:17:25 +0000

    Add indication of prize to view contest page

--- client/src/pages/prize/prize.vue
+++ client/src/pages/prize/prize.vue
@@ -1,11 +1,20 @@
 <template>
+<div class="prize-container">
+   
+      <span class="prize-value">{{ prize.value }}</span>
 
+     <span class="prize-type"> {{ prize.type }}</span>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'prize'
+  name: 'prize',
+  props: {
+    prize: Object
+  }
 }
+
 </script>
 
 <style scoped src='@/pages/prize/prize.css'>
