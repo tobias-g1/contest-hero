503697b307676d8a0c7bbb046de9b1cf7c4aedd6
Tobias <tobias.gooney@icloud.com>
2018-11-04 17:49:57 +0000

    Update validation for prizes fields

--- server/src/validation/contests.validation.js
+++ server/src/validation/contests.validation.js
@@ -11,19 +11,27 @@ module.exports = {
         category: Joi.string().required(),
         permlink: Joi.string().required(),
         entry_method: Joi.string().required(),
-        body: Joi.string().required()
+        body: Joi.string().required(),
+        prize: {
+          type: Joi.string().required(),
+          value: Joi.string().required()
+        }
     }
   },
   edit_contest: {
     body: {
-         access_token: Joi.string().min(6).max(512).required(),
+        access_token: Joi.string().min(6).max(512).required(),
         id: Joi.string().min(24).max(24).required(),
         title: Joi.string().required(),
         author: Joi.string().required(),
         deadline: Joi.string().required(),
         category: Joi.string().required(),
         entry_method: Joi.string().required(),
-        body: Joi.string().required()
+        body: Joi.string().required(),
+        prize: {
+          type: Joi.string().required(),
+          value: Joi.string().required()
+        }
     }
   },
   set_winners: {
