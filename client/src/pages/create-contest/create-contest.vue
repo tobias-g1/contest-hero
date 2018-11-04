d709e25270efc93dd333a1ab8ca1ec59f93b337a
Tobias <tobias.gooney@icloud.com>
2018-11-04 17:50:53 +0000

    Adjust create contest page to use mixin

--- client/src/pages/create-contest/create-contest.vue
+++ client/src/pages/create-contest/create-contest.vue
@@ -95,7 +95,7 @@
 import markdownEditor from 'vue-simplemde/src/markdown-editor'
 import form from '@/mixins/form-actions.js'
 import tags from '@/mixins/tags.js'
-import { mapGetters } from 'vuex'
+import contest from '@/mixins/contest.js'
 import contestsService from '@/services/contests.js'
 import tooltip from '@/components/tooltip/tooltip.vue'
 
@@ -103,7 +103,6 @@ export default {
   name: 'create-contest',
   data () {
     return {
-      labelPosition: 'top',
       inputVisible: false,
       inputValue: '',
       contestForm: {
@@ -116,92 +115,20 @@ export default {
         prizeType: '',
         prizeValue: ''
       },
-      contestId: '',
-      rules: {
-        title: [{
-          required: true,
-          message: 'Please enter a title',
-          trigger: 'blur'
-        }],
-        category: [{
-          required: true,
-          message: 'Please select a contest category',
-          trigger: 'change'
-        }],
-        entry_method: [{
-          required: true,
-          message: 'Please select an entry method',
-          trigger: 'change'
-        }],
-        deadline: [{
-          required: true,
-          message: 'Please select a deadline',
-          trigger: 'change'
-        }],
-        body: [{
-          required: true,
-          message: 'Please enter the body of your contest',
-          trigger: 'blur'
-        },
-        {
-          min: 150,
-          message: 'Your contest body should be at least 150 characters',
-          trigger: 'blur'
-        }
-        ],
-        prizeType: [{
-          required: true,
-          message: 'Please select a prize type',
-          trigger: 'blur'
-        }],
-        prizeValue: [{
-          message: 'Please select a prize value',
-          trigger: 'blur',
-          required: true
-        }]
-      },
-      editorConfig: {
-        spellChecker: true,
-        placeholder: 'Remember to add a description of your contest, any prizes you plan to offer and any additional information a reader should know about entering your contest. A user will automatically be able to enter your contest through the "Enter Contest" button shown on your contest page.'
-      }
+      contestId: ''
     }
   },
   components: {
     markdownEditor,
     tooltip
   },
-  mixins: [form, tags],
+  mixins: [form, tags, contest],
   computed: {
-    fixedTags: function () {
-      return ['contest-hero']
-    },
-    finalTags: function () {
-      return this.fixedTags.concat(this.contestForm.dynamicTags)
-    },
     contestPermlink: function () {
       return this.contestForm.title.toLowerCase().replace(/[\W]/g, '-') + '-' + Math.random().toString(36).replace(/[^a-z]+/g, '')
     },
-    postImages: function () {
-      let images = this.contestForm.body.match(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg|svg)/g)
-      if (images !== null) {
-        return images
-      } else {
-        return []
-      }
-    },
     adjustBody: function () {
       return this.contestForm.body + `<div><sub>This contest was created on <a  href="https://contesthero.io">Contest Hero</a> you can view and enter this contest by clicking <a target="blank_" href="https://www.contesthero.io/view-contest/${this.$store.state.steemconnect.user.name}/${this.contestPermlink})">here</a></sub></div>`
-    },
-    prizeType: function () {
-      return this.contestForm.prizeType
-    },
-    ...mapGetters('steemconnect', ['user'])
-  },
-  watch: {
-    prizeType () {
-      if (this.contestForm.prizeType === 'None') {
-        this.contestForm.prizeValue = ''
-      }
     }
   },
   methods: {
