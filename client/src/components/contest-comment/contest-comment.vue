92e0f012d4f5f727f7da655454d46471a6e6b474
Tobias <tobias.gooney@icloud.com>
2018-11-04 23:58:16 +0000

    Remove vue-markdown reference, replace with common markdown panel

--- client/src/components/contest-comment/contest-comment.vue
+++ client/src/components/contest-comment/contest-comment.vue
@@ -3,21 +3,22 @@
     <img class="comment-image" :src="commentProfileImage" alt="">
     <div class="comment-details">
       <span class="comment-author">{{ comment.author }}</span>
-      <VueMarkdown class="comment-body" :source="comment.body"></VueMarkdown>
+      <markdownpanel class="comment-body" :postbody="comment.body" />
       <postoptions :type="'comment'" :post="comment" />
     </div>
   </div>
 </template>
 
 <script>
-import VueMarkdown from 'vue-markdown'
+
 import postoptions from '@/components/post-options/post-options.vue'
+import markdownpanel from '@/components/markdown-panel/markdown-panel.vue'
 
 export default {
   name: 'comment',
   components: {
-    VueMarkdown,
-    postoptions
+    postoptions,
+    markdownpanel
   },
   props: {
     comment: Object
