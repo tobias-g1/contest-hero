03915f39daedcd0dfbe6930d3528ac6607dc9271
Tobias <tobias.gooney@icloud.com>
2018-11-04 20:56:18 +0000

    Trigger reblog pop up on click of icon, fix issue with loading indicator never stopping

--- client/src/components/post-options/post-options.vue
+++ client/src/components/post-options/post-options.vue
@@ -5,7 +5,7 @@
   <div v-if="post.active_votes" class="stats-container">
     <i class="material-icons stat-option vote vote-pulse" :class="{ voted: voted }" @click=" (user) ? dialogVisible = true : ''">favorite</i> <span class="icon-label">{{ post.net_votes }}</span>
     <i class="material-icons stat-option">attach_money</i> <span class="icon-label">{{post.pending_payout_value.slice(0, 3) }}</span>
-    <i v-if="type === 'full'" class="material-icons stat-option reblog vote-pulse">repeat</i>
+    <i v-if="type === 'full'" @click="resteemVisible = true" class="material-icons stat-option reblog vote-pulse">repeat</i>
 
     <!-- Voting Dialog -->
 
@@ -96,6 +96,7 @@ export default {
           message: 'You have successfully reblogged this post',
           type: 'success'
         })
+        this.$store.commit('setLoading', false)
       })
     }
   }
